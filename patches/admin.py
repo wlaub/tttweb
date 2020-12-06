4e3a90c9bf0685c1b1adf85b964e8984728c2a16
William Laub <goldcray@seminole.dreamhost.com>
2020-12-05 16:42:52 -0800

    admin stuff

--- patches/admin.py
+++ patches/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
 
+from . import models
 from .models import PatchEntry
-# Register your models here.
-admin.site.register(PatchEntry)
+class AuthorInline(admin.StackedInline):
+    model = models.PatchAuthor
+    extra = 1
+
+class ImageInline(admin.StackedInline):
+    model = models.PatchImages
+    extra = 1
+
+class AttachmentInline(admin.StackedInline):
+    model = models.PatchAttachments
+    extra = 0
+
+class TagInline(admin.StackedInline):
+    model = models.PatchTag
+    extra = 1
+
+class RepoInline(admin.TabularInline):
+    model = models.PatchRepoAttachment
+    extra = 1
+
+class PatchAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields':['name', 'desc', 'recording', 'date']}),
+        ]
+    inlines = [AuthorInline, ImageInline, TagInline, RepoInline, AttachmentInline]
+
+admin.site.register(PatchEntry, PatchAdmin)
+
