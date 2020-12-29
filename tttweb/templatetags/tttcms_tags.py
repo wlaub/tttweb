6359fb40a55b94cc75f1736e157b8820fa6849a3
wlaub <goldcray@gmail.com>
2020-12-29 15:30:10 -0800

    markdown in description

--- tttweb/templatetags/tttcms_tags.py
+++ tttweb/templatetags/tttcms_tags.py
@@ -1,9 +1,15 @@
 from django import template
+import markdown
 
 register = template.Library()
 
 @register.filter(name='is_expanded')
 def is_expanded(value):
     return value.ancestor or value.selected
+
+@register.filter(name='render_markdown')
+def render_markdown(value):
+    md = markdown.Markdown(extensions=['extra', 'sane_lists', 'nl2br'])
+    return md.convert(value)
 
 
