c887d00c1a2d9a02815963d46062a5f7142cccbb
wlaub <goldcray@gmail.com>
2021-01-02 15:40:41 -0800

    starting feeds

--- patches/urls.py
+++ patches/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'patches'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('rss/', views.IndexFeed(), name='rss'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('compare/', views.CompareView.as_view(), name='compare'),
     path('compare/<int:pk>/', views.CompareView.as_view(), name='compare'),
