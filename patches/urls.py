afe70bd495039e0cdbb7b320ee8d00fe4e5d6583
wlaub <goldcray@gmail.com>
2021-01-05 18:02:02 -0800

    Converted author link to manytomany field

--- patches/urls.py
+++ patches/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
@@ -9,6 +9,11 @@ urlpatterns = [
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('compare/', views.CompareView.as_view(), name='compare'),
     path('compare/<int:pk>/', views.CompareView.as_view(), name='compare'),
-    path('tags/', views.TagView.as_view(), name='tags')
+    path('tags/', views.TagView.as_view(), name='tags'),
+
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/entries', views.PatchEntryAPIList.as_view(), name='entry-list-api'),
+    path('api/entries/<int:pk>', views.PatchEntryAPIDetail.as_view(), name='entry-api'),   
+
 ]
 
