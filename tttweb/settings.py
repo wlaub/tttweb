a533d931afefd06477c2525e72c0a5c51a568dc1
wlaub <goldcray@gmail.com>
2021-06-17 13:39:45 -0700

    paginate entry api responses just in case

--- tttweb/settings.py
+++ tttweb/settings.py
@@ -187,7 +187,9 @@ THUMBNAIL_PROCESSORS = (
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES'    : [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-        ]
+        ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
     }
 
 
